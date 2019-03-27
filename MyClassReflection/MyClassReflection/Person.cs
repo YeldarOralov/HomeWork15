fdb5b63e90830f7bdaf7d37774ac4157e5ea1b45
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-03-27 10:04:08 +0600

    Добавил новое решение MyClassReflection, где создал класс Person.

---
+++ MyClassReflection/MyClassReflection/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassReflection
+{
+    public class Person
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ MyClassReflection/MyClassReflection/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person()
+            {
+                FullName = "Mark Jacobs",
+                Age = 28,
+                Height = 180,
+                Weight = 77,
+                Country = "USA"
+            };
+
+            var type = person.GetType();
+            //Console.WriteLine(person.GetType().GetMembers());
+            foreach (var attributes in type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+            {
+                Console.WriteLine(attributes.GetValue(person));
+            }
+                    
+
+            Console.Read();
+        }
+    }
+}
