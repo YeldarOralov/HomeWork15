f96b13e80dd0f2e2ea095cae674cef8f6083c986
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-03-27 09:29:21 +0600

    Добавлены начальные файлы проекта.

---
+++ ConsoleClass/ConsoleClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = typeof(Console);
+            foreach(var method in type.GetMethods())
+            {
+                Console.WriteLine($"{method}");
+            }
+            Console.Read();
+        }
+    }
+}
